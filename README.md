0993f9b6f0b8b399fc2ab2903a22b08aa35c84e9
nngocsonk3 <sonnn.21ce@vku.udn.vn>
2025-06-12 18:03:39 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# MTCNN-and-Facenet-Face-Recognition-System-for-Arduino-and-Esp32-Control
+<h1 align="center"></h1>
+<h3 align="center">MTCNN and Facenet Face Recognition System for Arduino and Esp32 Control</h3>
+<img align="right" alt="Coding" width="400" src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif">
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=nngocsonk3&label=Profile%20views&color=0e75b6&style=flat" alt="nngocsonk3" /> </p>
+
+<p align="left"> <a href="https://twitter.com/" target="blank"><img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
+
+- 🌱 **Vietnam Korea University of Information and Communication Technology**
+- 📫 How to reach me **sonnn.21ce@vku.udn.vn**
+- 👨‍💻 Team: Ngô Ngọc Sơn.
+<h1 align="center"></h1>
+<h3 align="center">Navigation-HMI-System-for-Automotive</h3>
+<img align="right" alt="Coding" width="400" src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif">
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=nngocsonk3&label=Profile%20views&color=0e75b6&style=flat" alt="nngocsonk3" /> </p>
+
+<p align="left"> <a href="https://twitter.com/" target="blank"><img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
+
+- 🌱 **Vietnam Korea University of Information and Communication Technology**
+- 📫 How to reach me **sonnn.21ce@vku.udn.vn**
+- 👨‍💻 Team: Ngô Ngọc Sơn.
---
+++ src/face_arduino.py
@@ -0,0 +1,150 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from imutils.video import VideoStream
+
+import argparse
+import facenet
+import imutils
+import os
+import sys
+import math
+import pickle
+import align.detect_face
+import numpy as np
+import cv2
+import collections
+from sklearn.svm import SVC
+import serial
+import time
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help='Path of the video you want to test on.', default=0)
+    args = parser.parse_args()
+
+    MINSIZE = 20
+    THRESHOLD = [0.6, 0.7, 0.7]
+    FACTOR = 0.709
+    IMAGE_SIZE = 182
+    INPUT_IMAGE_SIZE = 160
+    CLASSIFIER_PATH = 'Models/facemodel.pkl'
+    VIDEO_PATH = args.path
+    FACENET_MODEL_PATH = 'Models/20180402-114759.pb'
+
+    # Load The Custom Classifier
+    with open(CLASSIFIER_PATH, 'rb') as file:
+        model, class_names = pickle.load(file)
+    print("Custom Classifier, Successfully loaded")
+
+    # Khởi tạo kết nối Serial với Arduino
+    try:
+        ser = serial.Serial('COM4', 9600)  # Thay 'COM3' bằng cổng COM của Arduino
+        print("Kết nối Arduino thành công")
+    except serial.SerialException:
+        print("Lỗi kết nối Arduino. Vui lòng kiểm tra lại cổng COM và kết nối.")
+        sys.exit(1)
+
+    with tf.Graph().as_default():
+        gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.6)
+        sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+
+        with sess.as_default():
+            print('Loading feature extraction model')
+            facenet.load_model(FACENET_MODEL_PATH)
+
+            images_placeholder = tf.compat.v1.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.compat.v1.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.compat.v1.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = embeddings.get_shape()[1]
+
+            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, "src/align")
+
+            people_detected = set()
+            person_detected = collections.Counter()
+
+            cap = VideoStream(src=0).start()
+
+            # Biến để lưu thời gian bắt đầu nhận diện
+            start_time = 0
+            # Biến để kiểm tra xem đã gửi tín hiệu đến Arduino chưa
+            sent = False
+
+            while (True):
+                frame = cap.read()
+                frame = imutils.resize(frame, width=600)
+                frame = cv2.flip(frame, 1)
+
+                bounding_boxes, _ = align.detect_face.detect_face(frame, MINSIZE, pnet, rnet, onet, THRESHOLD, FACTOR)
+
+                faces_found = bounding_boxes.shape[0]
+                try:
+                    if faces_found > 1:
+                        cv2.putText(frame, "Only one face", (0, 100), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                    1, (255, 255, 255), thickness=1, lineType=2)
+                    elif faces_found > 0:
+                        det = bounding_boxes[:, 0:4]
+                        bb = np.zeros((faces_found, 4), dtype=np.int32)
+                        for i in range(faces_found):
+                            bb[i][0] = det[i][0]
+                            bb[i][1] = det[i][1]
+                            bb[i][2] = det[i][2]
+                            bb[i][3] = det[i][3]
+
+                            if (bb[i][3]-bb[i][1])/frame.shape[0]>0.25:
+                                cropped = frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :]
+                                scaled = cv2.resize(cropped, (INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE),
+                                                    interpolation=cv2.INTER_CUBIC)
+                                scaled = facenet.prewhiten(scaled)
+                                scaled_reshape = scaled.reshape(-1, INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE, 3)
+                                feed_dict = {images_placeholder: scaled_reshape, phase_train_placeholder: False}
+                                emb_array = sess.run(embeddings, feed_dict=feed_dict)
+
+                                predictions = model.predict_proba(emb_array)
+                                best_class_indices = np.argmax(predictions, axis=1)
+                                best_class_probabilities = predictions[
+                                    np.arange(len(best_class_indices)), best_class_indices]
+                                best_name = class_names[best_class_indices[0]]
+                                print("Name: {}, Probability: {}".format(best_name, best_class_probabilities))
+
+                                if best_class_probabilities > 0.8:
+                                    cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)
+                                    text_x = bb[i][0]
+                                    text_y = bb[i][3] + 20
+
+                                    name = class_names[best_class_indices[0]]
+                                    cv2.putText(frame, name, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                                1, (255, 255, 255), thickness=1, lineType=2)
+                                    cv2.putText(frame, str(round(best_class_probabilities[0], 3)), (text_x, text_y + 17),
+                                                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                                1, (255, 255, 255), thickness=1, lineType=2)
+                                    person_detected[best_name] += 1
+
+                                    # **Đoạn code xử lý thời gian và gửi tín hiệu**
+                                    if start_time == 0:  # Bắt đầu đếm khi nhận diện được khuôn mặt
+                                        start_time = time.time()
+                                    else:  # Nếu đã bắt đầu đếm, kiểm tra xem khuôn mặt có còn được nhận diện không
+                                        elapsed_time = time.time() - start_time
+                                        if elapsed_time >= 5 and not sent:
+                                            ser.write(b'1')
+                                            print("Đã gửi số 1 đến Arduino")
+                                            sent = True
+                                else:
+                                    start_time = 0  # Reset bộ đếm nếu không nhận diện được khuôn mặt
+                                    sent = False
+                                    name = "Unknown"
+
+                except:
+                    pass
+
+                cv2.imshow('Face Recognition', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+            ser.close()
+
+main()
